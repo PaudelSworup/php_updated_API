a497ddf000260ce475a5e13df698b8a92b88039a
PaudelSworup <sworupkc9@gmail.com>
2022-06-02 21:14:40 +0545

    updated

--- vehicleRequest.php
+++ vehicleRequest.php
@@ -15,8 +15,20 @@ if(isset($_POST)){
         echo "connected";
     }
 
-    $insert = "insert into vehiclerequest(name,image,rating,source,destination,rentalday,firebase_id) values ('$name','$image','$rating','$source','$dest','$days','$userID')";
+   $query = "select * from vehiclerequest where name ='".$name."' and firebase_id='".$userID."'";
+    $res =  mysqli_query($con,$query);
+
+   $num = mysqli_num_rows($res);
+
+   if($num > 0){
+       echo "You alread made a vehicle request of ".$name;
+   }else{
+    $insert = "insert into vehiclerequest(name,image,rating,source,destination,rentalday,firebase_id) values ('$name','$image','$rating','$src','$dest','$days','$userID')";
     mysqli_query($con,$insert);
+
+   }
+
+   
 
 
 }
